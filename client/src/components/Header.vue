c205d538414819395d0df61641717d9d486abb4f
TatiParhom <tvparhom@bk.ru>
2024-08-28 21:19:12 +0300

    в классе

---
+++ client/src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <header>
+        <h1>Полезные свойства капусты</h1>
+        <nav>
+            
+        </nav>
+        <button>
+
+        </button>
+
+    </header>
+</template>
